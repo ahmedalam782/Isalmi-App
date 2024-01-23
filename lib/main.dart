fb4e717af4d52f7d45bf8a6f3b130d0429b1aebd
ahmedmohamed <ahmedalam4887482@gmail.com>
2024-01-23 11:07:12 +0200

    create radio screen make Themes dark and light

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:islami_app/layout/home_screen.dart';
 import 'package:islami_app/layout/splash_screen.dart';
+import 'package:islami_app/shared/theme_app.dart';
 
 import 'modules/hadith_details_screen/hadith_datails_screen.dart';
 import 'modules/sura_details_screen/sura_datails_sreen.dart';
@@ -15,43 +16,15 @@ class IslamiApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       routes: {
-        SplashScreen.routeName: (_) => SplashScreen(),
-        HomeScreen.routeName: (_) => HomeScreen(),
+        SplashScreen.routeName: (_) => const SplashScreen(),
+        HomeScreen.routeName: (_) => const HomeScreen(),
         SuraDetailsScreen.routeName: (_) => SuraDetailsScreen(),
         HadithDetailsScreen.routeName: (_) => HadithDetailsScreen(),
       },
       initialRoute: SplashScreen.routeName,
-      theme: ThemeData(
-          progressIndicatorTheme: ProgressIndicatorThemeData(
-            color: Color(0xffB7935F),
-          ),
-          primaryColor: Color(0xffB7935F),
-          scaffoldBackgroundColor: Colors.transparent,
-          appBarTheme: AppBarTheme(
-            iconTheme: IconThemeData(
-              color: Colors.black,
-            ),
-            color: Colors.transparent,
-            elevation: 0,
-            titleTextStyle: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-              color: Color(0xff242424),
-              fontFamily: 'El Messiri',
-            ),
-            centerTitle: true,
-          ),
-          bottomNavigationBarTheme: BottomNavigationBarThemeData(
-              selectedItemColor: Color(0xff242424),
-              unselectedItemColor: Colors.white,
-              selectedIconTheme: IconThemeData(
-                color: Color(0xff242424),
-              ),
-              selectedLabelStyle: TextStyle(
-                fontSize: 14,
-                fontFamily: 'JF Flat',
-                color: Color(0xff242424),
-              ))),
+      theme: ThemeDataApp.lightTheme,
+      darkTheme: ThemeDataApp.darkTheme,
+      themeMode: ThemeMode.light,
     );
   }
 }
